47a4cb14355fe31ce3a5c5413e58e3cdbe9a1687
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2023-02-27 22:38:37 +0300

    test_data_structure

--- tests/test_earthquake.py
+++ tests/test_earthquake.py
@@ -4,6 +4,7 @@ from assertpy import assert_that
 from session import Endpoints, RequestTypes, StatusCodes
 
 ENDPOINT = Endpoints.EARTHQUAKE
+DATA_STRUCTURE = ['timestamp', 'latitude', 'longitude', 'depth', 'size', 'quality', 'humanReadableLocation']
 
 
 @allure.feature('Earthquake')
@@ -13,3 +14,17 @@ class TestEarthquake:
     def test_api_status_code(self, http_object):
         status_code, _ = http_object.send_request(RequestTypes.GET, ENDPOINT)
         assert_that(status_code).is_equal_to(StatusCodes.HTTP_OK)
+
+    @allure.title('Test API data structure')
+    def test_data_structure(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(type(data)).is_equal_to(dict)
+        assert_that('results' in data.keys()).is_true()
+        assert_that(all([isinstance(record, dict) for record in data['results']])).is_true()
+
+        for key in DATA_STRUCTURE:
+            assert_that(all(key in record.keys() for record in data['results'])).is_true()
+
+        for record in data['results']:
+            assert_that(all(key in DATA_STRUCTURE for key in record.keys())).is_true()
