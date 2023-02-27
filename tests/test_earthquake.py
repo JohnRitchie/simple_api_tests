6762948f2933e88763147deb15981b40c93459c6
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2023-02-27 22:41:26 +0300

    tests data format

--- tests/test_earthquake.py
+++ tests/test_earthquake.py
@@ -1,6 +1,8 @@
 import pytest
 import allure
 from assertpy import assert_that
+from datetime import datetime
+import re
 
 from session import Endpoints, RequestTypes, StatusCodes
 
@@ -39,3 +41,48 @@ class TestEarthquake:
         _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
 
         assert_that(all([isinstance(record[entity], entity_type) for record in data['results']])).is_true()
+
+    @allure.title('Test timestamp format')
+    def test_timestamp_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([isinstance(datetime.strptime(record['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ'), datetime)
+                         for record in data['results']])).is_true()
+
+    @allure.title('Test latitude format')
+    def test_latitude_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([62 < record['latitude'] < 67 for record in data['results']])).is_true()
+
+    @allure.title('Test longitude format')
+    def test_longitude_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([-30 < record['longitude'] < -11 for record in data['results']])).is_true()
+
+    @allure.title('Test depth format')
+    def test_depth_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([0 < record['depth'] < 800 for record in data['results']])).is_true()
+
+    @allure.title('Test size format')
+    @pytest.mark.xfail(reason="The data contains a negative size")
+    def test_size_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([0 < record['size'] < 9 for record in data['results']])).is_true()
+
+    @allure.title('Test quality format')
+    def test_quality_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([0 < record['quality'] < 100 for record in data['results']])).is_true()
+
+    @allure.title('Test humanReadableLocation format')
+    def test_humanReadableLocation_format(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, ENDPOINT)
+
+        assert_that(all([re.match(r'^\d+,\d+ km [A|S|N|V]{1,3} af .*$', record['humanReadableLocation']) is not None
+                         for record in data['results']])).is_true()
